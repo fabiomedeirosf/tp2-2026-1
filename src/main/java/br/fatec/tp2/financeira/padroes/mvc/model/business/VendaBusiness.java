957a525b9d8816adc14649b09c28c7c6cc3ae4eb
Fabio Faria <fabiomedeirosf@gmail.com>
2026-06-13 11:00:11 -0300

    feat: mvc demo

---
+++ src/main/java/br/fatec/tp2/financeira/padroes/mvc/model/business/VendaBusiness.java
@@ -0,0 +1,8 @@
+package br.fatec.tp2.financeira.padroes.mvc.model.business;
+
+import br.fatec.tp2.financeira.padroes.mvc.model.dto.Venda;
+
+public interface VendaBusiness {
+
+	public Venda criarVenda(Venda venda);
+}
